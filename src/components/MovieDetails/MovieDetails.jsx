b9e5200600610199878bb31ddf719fd3ff809555
TheLazySock <asd2256501@gmail.com>
2018-10-09 17:16:26 +0300

    --amend

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-// import style from './MovieDetails.scss';
+import style from './MovieDetails.scss';
 
 const MovieDetails = () => (
-  <section>
+  <section className={style.movieDetailsContainer}>
     {/* test */}
   </section>
 );
