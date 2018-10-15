0b87d63c4854fdc99bac107389b6aaba92f343b8
TheLazySock <asd2256501@gmail.com>
2018-10-15 12:33:35 +0300

    Genres destructurization added, also removed genresItems variable

--- src/components/MovieDetails/MovieDetailsInfo/Genres/Genres.jsx
+++ src/components/MovieDetails/MovieDetailsInfo/Genres/Genres.jsx
@@ -2,16 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import style from './Genres.scss';
 
-const Genres = (props) => {
-  const { genres } = props;
-  const genresItems = genres.map(item => <li className={style.genresItem} key={item}>{item}</li>);
-
-  return (
-    <ul className={style.genres}>
-      {genresItems}
-    </ul>
-  );
-};
+const Genres = ({ genres }) => (
+  <ul className={style.genres}>
+    {genres.map(item => <li className={style.genresItem} key={item}>{item}</li>)}
+  </ul>
+);
 
 Genres.propTypes = {
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
