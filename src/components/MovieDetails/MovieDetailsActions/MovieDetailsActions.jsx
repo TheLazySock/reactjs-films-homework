f2280e34ed16daf173b21c2d29888c920d10b698
TheLazySock <asd2256501@gmail.com>
2018-10-15 12:30:58 +0300

    MovieDetailsActions destructurization added

--- src/components/MovieDetails/MovieDetailsActions/MovieDetailsActions.jsx
+++ src/components/MovieDetails/MovieDetailsActions/MovieDetailsActions.jsx
@@ -5,16 +5,12 @@ import style from './MovieDetailsActions.scss';
 import Button from '../../Button';
 import ViewInfo from './ViewInfo';
 
-const MovieDetailsActions = (props) => {
-  const { info } = props;
-
-  return (
-    <div className={style.movieDetailsActions}>
-      <Button buttonClass="watchVideoBtn" title="Watch Now" />
-      <ViewInfo info={info} />
-    </div>
-  );
-};
+const MovieDetailsActions = ({ info }) => (
+  <div className={style.movieDetailsActions}>
+    <Button buttonClass="watchVideoBtn" title="Watch Now" />
+    <ViewInfo info={info} />
+  </div>
+);
 
 MovieDetailsActions.propTypes = {
   info: PropTypes.string.isRequired,
