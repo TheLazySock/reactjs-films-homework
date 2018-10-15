2921f921096309e022290b2af75708381f130a1f
TheLazySock <asd2256501@gmail.com>
2018-10-15 12:35:11 +0300

    MovieDetailsInfo destructurization added

--- src/components/MovieDetails/MovieDetailsInfo/MovieDetailsInfo.jsx
+++ src/components/MovieDetails/MovieDetailsInfo/MovieDetailsInfo.jsx
@@ -5,22 +5,18 @@ import style from './MovieDetailsInfo.scss';
 import Rating from './Rating';
 import Genres from './Genres';
 
-const MovieDetailsInfo = (props) => {
-  const {
-    title, genres, duration, rating,
-  } = props;
-
-  return (
-    <div className={style.movieInfo}>
-      <h2 className={style.movieInfoTitle}>{title.toUpperCase()}</h2>
-      <div className={style.genresDurationBox}>
-        <Genres genres={genres} />
-        <span className={style.duration}>{duration}</span>
-      </div>
-      <Rating rating={rating} />
+const MovieDetailsInfo = ({
+  title, genres, duration, rating,
+}) => (
+  <div className={style.movieInfo}>
+    <h2 className={style.movieInfoTitle}>{title.toUpperCase()}</h2>
+    <div className={style.genresDurationBox}>
+      <Genres genres={genres} />
+      <span className={style.duration}>{duration}</span>
     </div>
-  );
-};
+    <Rating rating={rating} />
+  </div>
+);
 
 MovieDetailsInfo.defaultProps = {
   genres: ['Adventure', 'Drama', 'Family', 'Fantasy'],
